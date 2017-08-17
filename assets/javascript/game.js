f8b5d9d009e7954850234a7eff566a181292e47f
michaelgalt1984 <michael.galt.1984@gmail.com>
2017-08-17 16:13:46 -0500

    Worked with tutor, got most of the things working. Need to see if I can straighten out the form entry and user guess log.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,37 +1,40 @@
 var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var randomLetter = null; 
+var randomLetter; 
 //this will be a function, which randomly draws a letter from the alphabet array
-var userGuess = []; 
+var userGuess; 
 //this will be a onkeyup capture from inside the form box
 var missedGuesses = []; 
 //this will be all the letters which the user guessed which didn't match the randomLetter, also max length of 3
 var guessesRemaining = ["3", "2", "1", "No more guesses left- you lose!"]; 
 //this is an array which get draws from if userGuess === false
+var guessIndex = 0;
 var wins = 0;
 //this will get iterated if userGuess === randomLetter
 var losses = 0;
 //this will get iterated if guessesRemaining === "No more guesses left- you lose!"
-var text = "";
+// var text = "";
 
 var updateGuessesRemaining = function() {
-	document.querySelector("guessesRemainingBox").innerHTML = guessesRemaining[i];
+	document.getElementById("guessesRemainingBox").innerHTML = guessesRemaining[guessIndex];
+	console.log(guessIndex);
 };
 
 var updateRandomLetter = function() {
 	randomLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
+	console.log(randomLetter);
 };
 
 var updateMissedGuesses = function() {
-	document.querySelector("guessBoxFeedback").innerHTML = missedGuesses.join(", ");
+	document.getElementById("guessBoxFeedback").innerHTML = missedGuesses.push(userGuess);
 };
 
-function getGuesses() {
-	var userGuess = document.getElementById("letterGuesses").value;
-};
+// function getGuesses() {
+// 	var userGuess = document.getElementById("letterGuesses").value;
+// };
 
 var reset = function() {
-	guessesRemaining = 0;
-	userGuess = [];
+	guessIndex = 0;
+	userGuess;
 	missedGuesses = [];
 	updateRandomLetter();
 	updateGuessesRemaining();
@@ -39,33 +42,39 @@ var reset = function() {
       // randomLetter = new;
   };
 
-updateRandomLetter();
-updateGuessesRemaining();
 
-document.onkeyup = function(event) {
-	guessesRemaining++;
-	var letter = String.fromCharCode(event.keyCode).toLowerCase();
-	userGuess.push(event.keyCode);
-	guessesRemaining();
-	updateGuessesSoFar();
-
-	if (letter === randomLetter) {
-
-	wins++;
-	document.querySelector("wins").innerHTML = wins;
-
-	reset();
-	}
+  document.addEventListener("DOMContentLoaded", function(event) { 
+  //do work
+  reset();
+  document.onkeyup = function(event) {
+  	userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+  	console.log(userGuess);
 
 
-	if (guessesRemaining === "No more guesses left- you lose!") {
+  	if (userGuess === randomLetter) {
 
-	losses++;
-	document.querySelector("losses").innerHTML = losses;
+  		wins++;
+  		document.getElementById("wins").innerHTML = wins;
 
-	reset();
-	}
-};
+  		reset();
+  	}
 
+  	else {
+  		
+  		if (guessIndex === 4) {
+
+  			losses++;
+  			document.getElementById("losses").innerHTML = losses;
+
+  			reset();
+  		}
+  		updateGuessesRemaining();
+  		updateMissedGuesses();
+  		guessIndex++;
+  	}
+
+
+  };
+});
 
 
